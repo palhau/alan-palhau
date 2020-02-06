95663ae5183b092a96fb0eb4ab78c4dad4109780
Alan Palhau <alanvieirapalhau@hotmail.com>
2020-02-06 20:56:36 -0300

    initial boilerplate

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
+function compilaSass() {
+  return gulp.src('css/scss/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream());
+}
+
+gulp.task('sass', compilaSass);
+
+function gulpJS() {
+  return gulp.src('js/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.stream())
+}
+
+gulp.task('mainjs', gulpJS);
+
+function browser() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  })
+}
+
+gulp.task('browser-sync', browser);
+
+function watch() {
+  gulp.watch('css/scss/*.scss', compilaSass);
+  gulp.watch('js/main/*.js', gulpJS);
+  gulp.watch('*.html').on('change', browserSync.reload);
+}
+
+gulp.task('watch', watch);
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync', 'sass', 'mainjs'));
